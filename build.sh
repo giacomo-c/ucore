00469bf78edfd3513193a3b3919e5554916aa612
giacomo-c <69265203+giacomo-c@users.noreply.github.com>
2025-06-21 15:13:25 -0400

    add:  nvidia-ctk cdi generate

--- build.sh
+++ build.sh
@@ -29,3 +29,7 @@ ln -rs /usr/bin/rclone /usr/bin/rclonefs
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+
+# needed for nvidia gpu container useage
+
+nvidia-ctk cdi generate
